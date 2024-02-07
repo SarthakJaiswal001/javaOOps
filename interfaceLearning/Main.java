2fe20003bf956c54a54f7f989d05e1402a08e616
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2024-02-07 17:57:59 +0530

    Update Main.java

--- interfaceLearning/Main.java
+++ interfaceLearning/Main.java
@@ -28,4 +28,7 @@ mstCar.stopMusic();
 //in abstract classes we cannot create abstract constructors
 //interfaces can have default and static methods as well
 //in interfaces variables are final whereas in abstract classes they can be final or nonfinal
-//in the interface the variables are final because if we don't have constructors then it can't be initialized, so final variable are compulsory to initialize. 
+//in the interface the variables are final because if we don't have constructors then it can't be initialized, so final variables are compulsory to initialize.
+//abstract classes can provide an implementation of the interface but the interface cannot provide an implementation of an abstract class.
+//in the interface we can use public only
+
