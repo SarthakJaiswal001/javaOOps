682185d0290ee403bf44bc4e95006f26065f4c54
SarthakJaiswal001 <96002671+SarthakJaiswal001@users.noreply.github.com>
2024-02-07 18:15:25 +0530

    Update Main.java

--- interfaceLearning/Main.java
+++ interfaceLearning/Main.java
@@ -31,4 +31,10 @@ mstCar.stopMusic();
 //in the interface the variables are final because if we don't have constructors then it can't be initialized, so final variables are compulsory to initialize.
 //abstract classes can provide an implementation of the interface but the interface cannot provide an implementation of an abstract class.
 //in the interface we can use public only
+//parent classes do not have an idea about child classes but child classes have an idea about parent.
+//if a function from the child class is called which is also present in the parent class then both are called at compile time.
+//interfaces also help in implementing hybrid inheritance, where two unrelated classes can implement a single interface class.
+//a interface can extend other interfaces only
+//abstract class can extend Java class and implement multiple interfaces as well.
+
 
